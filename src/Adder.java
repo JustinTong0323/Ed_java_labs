5c615df670665c297d59559906961caca00f523e
XinyuanTong <115166877+JustinTong0323@users.noreply.github.com>
2023-01-16 13:57:31 +0000

    finish week1 labs 16/1

---
+++ src/Adder.java
@@ -0,0 +1,8 @@
+public class Adder {
+    public static void main(String[] args) {
+        int MyNumber = Integer.parseInt(args[0]);
+        int MyNumber2 = Integer.parseInt(args[1]);
+        System.out.println(MyNumber + MyNumber2);
+
+    }
+}
---
+++ src/Multiplier.java
@@ -0,0 +1,9 @@
+public class Multiplier {
+    public static void main(String[] args) {
+        int MyNumber = Integer.parseInt(args[0]);
+        int MyNumber2 = Integer.parseInt(args[1]);
+        int MyNumber3 = Integer.parseInt(args[2]);
+        System.out.println(MyNumber + " * " + MyNumber2 +" * " + MyNumber3 + " = " + (MyNumber * MyNumber2 * MyNumber3));
+
+    }
+}
